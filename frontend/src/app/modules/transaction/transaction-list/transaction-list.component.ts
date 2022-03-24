5ae12c607f34878662c722f4279aa9b1a5bbc24f
Miguel Silva <miguelsilv98@gmail.com>
2022-03-24 14:29:50 -0300

    listagem das transações

---
+++ frontend/src/app/modules/transaction/transaction-list/transaction-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Transaction } from 'src/app/shared/models/transaction.model';
+import { TransactionsService } from '../transactions.service';
+
+@Component({
+  selector: 'app-transaction',
+  templateUrl: './transaction-list.component.html',
+  styleUrls: ['./transaction-list.component.scss']
+})
+export class TransactionListComponent implements OnInit {
+
+  public transactions: Transaction[];
+
+  constructor(
+    private readonly transactionsService: TransactionsService
+  ) {
+    this.transactions = []
+  }
+
+  ngOnInit(): void {
+    this.transactionsService.getTransactions()
+      .subscribe(transactions => {
+        this.transactions = transactions;
+      });
+  }
+
+}
