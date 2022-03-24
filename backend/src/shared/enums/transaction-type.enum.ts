d5c30490bd63b86349ed33afeb55de2df9310daa
Miguel Silva <miguelsilv98@gmail.com>
2022-03-24 10:55:57 -0300

    Tipos de transação

---
+++ backend/src/shared/enums/transaction-type.enum.ts
@@ -0,0 +1,14 @@
+
+export enum TransactionType {
+    Debit = 1,
+    Bill = 2,
+    Financing = 3,
+    Credit = 4,
+    Loan = 5,
+    Sales = 6,
+    ReceivementTED = 7,
+    ReceivementDOC = 8,
+    Rent = 9
+}
+
+
